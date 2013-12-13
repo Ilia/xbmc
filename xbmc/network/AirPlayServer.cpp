885a12c80771aea793e03abe2fd157545cb09812
Ilia Mogilevsky <ilia@mogilevsky.com.au>
2013-12-14 08:13:22 +1100

    airplay test fix

--- xbmc/network/AirPlayServer.cpp
+++ xbmc/network/AirPlayServer.cpp
@@ -120,7 +120,7 @@ const char *eventStrings[] = {"playing", "paused", "loading", "stopped"};
 "<key>deviceid</key>\r\n"\
 "<string>%s</string>\r\n"\
 "<key>features</key>\r\n"\
-"<integer>119</integer>\r\n"\
+"<integer>0x39f7</integer>\r\n"\
 "<key>model</key>\r\n"\
 "<string>Xbmc,1</string>\r\n"\
 "<key>protovers</key>\r\n"\
